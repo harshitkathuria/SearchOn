3c3e85fbf3aae3c6dc52b6e4271212907f956be2
harshitkathuria <harshitkathuria14@gmail.com>
2021-03-04 13:58:43 +0530

    Added NotFound Component

---
+++ src/components/NotFound.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const NotFound = () => {
+  return (
+    <div className="center" style={{ marginTop: "5rem" }}>
+      <div className="flow-text">Not Found</div>
+      <div>
+        <p className="lead">
+          The page that you are looking for does not exist...
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
